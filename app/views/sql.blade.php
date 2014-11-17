53efd6b5903f85d716eeae7999a90440a563ee4c
Alexeev Anton <alexeev.sker@gmail.com>
2014-11-17 16:31:07 +0300

    sql add table list

--- app/views/sql.blade.php
+++ app/views/sql.blade.php
@@ -2,19 +2,29 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-8">
         {{Form::open(array('url' => URL::to('/sql/')))}}
            <div class="form-group">
                {{Form::label(trans('mailing.groups'),'',array('class'=>'control-label'))}}
-               {{Form::textarea('q',Input::get('q',''))}}
+               {{Form::textarea('q',Input::get('q',''),array('class'=>'form-control'))}}
            </div>
             <div class="form-group">
-            {{ Form::hidden('place','mailer')}}
-            {{ Form::submit(trans('subscriber.create'),array('class'=>'btn btn-default'))}}
+                {{ Form::hidden('place','mailer')}}
+                {{ Form::submit(trans('subscriber.create'),array('class'=>'btn btn-default'))}}
             </div>
         {{Form::close()}}
     </div>
+    <div class="col-lg-4">
+        <b>Список табилц</b> <br>
+        <?
+        $tables = DB::select('SHOW TABLES');
+        foreach($tables as $table){
+            echo $table->Tables_in_mailer."<br>";
+        }
+        ?>
+    </div>
 </div>
+
 <div class="row">
     <div class="col-lg-12">
         <table class="table">
