547e3b6f6ac6250aeb931a3e292267c1ebd2364a
Alexeev Anton <alexeev.sker@gmail.com>
2014-12-11 14:40:17 +0300

    add unsubscribe link

--- app/views/emails/template.blade.php
+++ app/views/emails/template.blade.php
@@ -7,5 +7,6 @@
         {{$header}}
             {{$content}}
         {{$footer}}
+        <div style="text-align:center;"><a href="http://new.goodline.ru/unsubscribe.php?email={{$email}}">Отписаться от рассылки</a></div>
     </body>
 </html>
