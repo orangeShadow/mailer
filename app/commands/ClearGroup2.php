a5b26ebed46c49a47f2605d9a636a4315f640192
Alexeev Anton <alexeev.sker@gmail.com>
2015-04-02 14:37:35 +0300

    edit sql

--- app/commands/ClearGroup2.php
+++ app/commands/ClearGroup2.php
@@ -40,8 +40,9 @@ class ClearGroup2 extends Command {
         $k=0;
         $res =  DB::select(DB::raw("SELECT id,email FROM subscribers as s JOIN  subscriber_group as sg on  s.id = sg.subscriber_id  and sg.group_id in(45,46,47)"));
         foreach($res as $row){
+            $email = trim($row->email);
             $ids = array();
-            $els = DB::select(DB::raw("SELECT s.id  as id,email FROM subscribers as s JOIN  subscriber_group as sg on  s.id = sg.subscriber_id and s.email like '".$row->email."%' and sg.group_id in(2) LIMIT 50000"));
+            $els = DB::select(DB::raw("SELECT s.id  as id,email FROM subscribers as s JOIN  subscriber_group as sg on  s.id = sg.subscriber_id and s.email like '".$email."%' and sg.group_id in(2) LIMIT 50000"));
             foreach($els as $el)
             {
                 echo $el->email;
