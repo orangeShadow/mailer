e27b216550ac675fae1a4cf783547431afa831bf
Anton Alexeev <alexeev.sker@gmail.com>
2016-08-03 14:22:01 +0300

    Поменял label

--- app/views/group/csv.blade.php
+++ app/views/group/csv.blade.php
@@ -6,30 +6,30 @@
 
 @section('content')
     {{ Form::open(array('url' => URL::to('/group/fromCSV'),'class'=>"form-horizontal",'method'=>"POST",'files'=>true))}}
-        <div id="form-content">
-            <div class="form-group">
-                <label  class="col-sm-2 control-label">Тип:</label>
-                <div class="col-sm-10">
-                    {{Form::select('group_id',Group::lists('title','id'))}}
+            <div id="form-content">
+                <div class="form-group">
+                    <label  class="col-sm-2 control-label">Тип:</label>
+                    <div class="col-sm-10">
+                        {{Form::select('group_id',Group::lists('title','id'))}}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="form-content">
-            <div class="form-group">
-                <label  class="col-sm-2 control-label">Фаил:</label>
-                <div class="col-sm-10">
-                    {{Form::file('csv')}}
+            <div id="form-content">
+                <div class="form-group">
+                    <label  class="col-sm-2 control-label">Фаил:</label>
+                    <div class="col-sm-10">
+                        {{Form::file('csv')}}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="form-content">
-            <div class="form-group">
-                <label  class="col-sm-2 control-label">Тип:</label>
-                <div class="col-sm-10">
-                    {{Form::checkbox('clean',1)}}
+            <div id="form-content">
+                <div class="form-group">
+                    <label  class="col-sm-2 control-label">Очищать группу:</label>
+                    <div class="col-sm-10">
+                        {{Form::checkbox('clean',1)}}
+                    </div>
                 </div>
             </div>
-        </div>
             {{ Form::token()}}
             {{ Form::submit(trans('templates.create'),array('class'=>'btn btn-default'))}}
     {{ Form::close()}}
